a3650188b81b51bbc5824f777fa7a733d3306ba2
Jackson Zastera <jackson.zastera@trojans.dsu.edu>
2021-11-14 20:40:42 -0600

    Creation of Issue and Issue Repository Classes

---
+++ P5/Issue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Issue
+    {
+        int Id;
+        int ProjectId;
+        string Title;
+        DateTime dateTime;
+        string Discoverer;
+        string InitialDescription;
+        string Component;
+        int IssueStatusId;
+    }
+}
