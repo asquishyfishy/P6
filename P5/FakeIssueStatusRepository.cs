7db6b7f9a0afa95153167e06dffbddf672d173ec
Tim <timothy.sereda@trojans.dsu.edu>
2021-11-14 20:09:55 -0600

    Added IssueStatus class, new repo + interface

---
+++ P5/FakeIssueStatusRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace P5
+{
+    public class FakeIssueStatustRepository : IIssueStatusRepository
+    {
+        private static List<IssueStatus> _IssueStatuses = new List<IssueStatus>();
+        public FakeIssueStatustRepository()
+        {
+            if (_IssueStatuses.Count == 0)
+            {
+                int idToIgnore;
+                // Populate some temporary values to work with
+                
+            }
+        }
+        public void Add(int Id, string value)
+        {
+            
+        }
+        public List<IssueStatus> GetAll()
+        {
+            return _IssueStatuses;
+        }
+        public int GetIdByStatus(string value)
+        {
+            return 1;
+        }
+        public string GetValueById(int Id)
+        {
+            return "string";
+        }
+    }
+}
---
+++ P5/IssueStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace P5
+{
+    public class IssueStatus
+    {
+        int Id;
+
+        string Value;
+    }
+}
