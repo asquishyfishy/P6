b6584134d50b9e5fd6948e17f50518edc3d1ec1d
Jackson Zastera <jackson.zastera@trojans.dsu.edu>
2021-11-15 22:38:59 -0600

    Modify UI

---
+++ P5/FormIssueModify.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Builder
+{
+    partial class FormIssueModify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiscoveryDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Discoverer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InitialDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Component = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SelectIssuebutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Title,
+            this.DiscoveryDate,
+            this.Discoverer,
+            this.InitialDescription,
+            this.Component,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(946, 311);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 40;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.MinimumWidth = 6;
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            this.Title.Width = 175;
+            // 
+            // DiscoveryDate
+            // 
+            this.DiscoveryDate.HeaderText = "Discovery Date";
+            this.DiscoveryDate.MinimumWidth = 6;
+            this.DiscoveryDate.Name = "DiscoveryDate";
+            this.DiscoveryDate.ReadOnly = true;
+            this.DiscoveryDate.Width = 150;
+            // 
+            // Discoverer
+            // 
+            this.Discoverer.HeaderText = "Discoverer";
+            this.Discoverer.MinimumWidth = 6;
+            this.Discoverer.Name = "Discoverer";
+            this.Discoverer.ReadOnly = true;
+            this.Discoverer.Width = 125;
+            // 
+            // InitialDescription
+            // 
+            this.InitialDescription.HeaderText = "Initial Description";
+            this.InitialDescription.MinimumWidth = 6;
+            this.InitialDescription.Name = "InitialDescription";
+            this.InitialDescription.ReadOnly = true;
+            this.InitialDescription.Width = 150;
+            // 
+            // Component
+            // 
+            this.Component.HeaderText = "Componenet";
+            this.Component.MinimumWidth = 6;
+            this.Component.Name = "Component";
+            this.Component.ReadOnly = true;
+            this.Component.Width = 125;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            this.Status.Width = 130;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(627, 404);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(122, 34);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // SelectIssuebutton
+            // 
+            this.SelectIssuebutton.Location = new System.Drawing.Point(836, 404);
+            this.SelectIssuebutton.Name = "SelectIssuebutton";
+            this.SelectIssuebutton.Size = new System.Drawing.Size(122, 34);
+            this.SelectIssuebutton.TabIndex = 2;
+            this.SelectIssuebutton.Text = "Select Issue";
+            this.SelectIssuebutton.UseVisualStyleBackColor = true;
+            // 
+            // FormIssueModify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 450);
+            this.Controls.Add(this.SelectIssuebutton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormIssueModify";
+            this.Text = "Select Issue";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiscoveryDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Discoverer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InitialDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Component;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button SelectIssuebutton;
+    }
+}
